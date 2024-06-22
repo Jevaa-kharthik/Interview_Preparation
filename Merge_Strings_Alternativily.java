99ef25235fb07e859efb951dc198fade4808267b
Jevaa Kharthik N <113284998+Jevaa-kharthik@users.noreply.github.com>
2024-06-22 11:53:55 +0530

    Greatest Number

--- Merge_Strings_Alternativily.java
+++ Merge_Strings_Alternativily.java
@@ -2,10 +2,16 @@ public class Merge_Strings_Alternativily{
     public String merge_string(String word1, String word2){
         StringBuilder sb = new StringBuilder();
         for(int i=0; i<word1.length() || i<word2.length(); i++){
-            
+            if(i<word1.length()){
+                sb.append(word1.charAt(i));
+            }
+            if(i<word2.length()){
+                sb.append(word2.charAt(i));
+            }
         }
         return sb.toString();
     }
+    // public String merge_string(String word1, String word2){
     //     String result = "";
     //     if(word1.length() == word2.length()){
     //         for(int i=0;i < word1.length(); i++){
