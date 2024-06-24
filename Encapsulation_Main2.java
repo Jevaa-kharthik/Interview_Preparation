4b01e0159107afe8df4a9692b9fd9f810ca03e80
Jevaa Kharthik N <113284998+Jevaa-kharthik@users.noreply.github.com>
2024-06-24 21:41:46 +0530

    Encapsulation

---
+++ Encapsulation_Main2.java
@@ -0,0 +1,12 @@
+public class Encapsulation_Main2 {
+    private String Name;
+
+    public String getName(){
+        return Name;
+    }
+
+    public void setName(String name){
+        this.Name = name;
+    }
+    
+}
---
+++ Main2.java
@@ -0,0 +1,11 @@
+public class Main2 {
+    public static void main(String[] args) {
+        Encapsulation_Main2 obj = new Encapsulation_Main2();
+        obj.setName("Gayathri");
+        System.out.println(obj.getName());
+        obj.setName("Jevaa Kharthik N");
+        System.out.println(obj.getName());
+
+    }
+    
+}
