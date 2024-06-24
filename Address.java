7120cdb24fda2e71b2caece615e64087b13c0de0
Jevaa Kharthik N <113284998+Jevaa-kharthik@users.noreply.github.com>
2024-06-24 20:45:15 +0530

    OOPS Concept

---
+++ Address.java
@@ -0,0 +1,11 @@
+public class Address {
+    public String street;
+    public String city;
+    public int pinCode;
+    public String country;
+
+    public void displayAddress(){
+        System.err.println("Street : " + street + "City : " + city + "Pin Code : " + pinCode + "Country : " + country );
+    }
+    
+}
