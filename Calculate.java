99c1f0e39db707bb675b50ae50343b9f9e5791b6
Jevaa Kharthik N <113284998+Jevaa-kharthik@users.noreply.github.com>
2024-08-13 15:33:23 +0530

    Simple Program to cube

---
+++ Calculate.java
@@ -0,0 +1,10 @@
+public class Calculate {
+
+    static int cube(int x){
+        return x * x * x;
+    }
+    public static void main(String[] args) {
+        System.out.println(cube(5));
+    }
+    
+}
