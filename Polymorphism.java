979fc7ff924acf4fdfc2277a077f8b31146225c5
Jevaa Kharthik N <113284998+Jevaa-kharthik@users.noreply.github.com>
2024-08-13 18:47:12 +0530

    Simple Program to cube

--- Polymorphism.java
+++ Polymorphism.java
@@ -5,23 +5,23 @@ class Animal{
 }
 
 class pig extends Animal{
-    public void sound(){
-        System.out.println("The pigs make the sound like ");
+    public void sound(String nothing){
+        System.out.println("The pigs make the sound like " + nothing);
     }
 }
 
 class dogs extends Animal{
-    public void sound(){
-        System.out.println("The Dogs make sound like");
+    public void sound(String nothing){
+        System.out.println("The Dogs make sound like" + nothing);
     }
 }
 
 public class Polymorphism {
     public static void main(String[] args) {
         pig pig_sound = new pig();
-        pig_sound.sound();
+        pig_sound.sound("Ouchhhh");
         dogs dog_sound = new dogs();
-        dog_sound.sound();
+        dog_sound.sound("barkkkkk");
     }
     
 }
