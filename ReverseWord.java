cdbc104003c550c7abea46d126def90dd5602f6e
Jevaa Kharthik N <113284998+Jevaa-kharthik@users.noreply.github.com>
2024-09-03 20:59:11 +0530

    Interview Question

--- ReverseWord.java
+++ ReverseWord.java
@@ -10,8 +10,22 @@ public class ReverseWord {
         
         return sb.toString().trim();
     }
+    public static String Vowel_Capital(String sentence){
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < sentence.length(); i++){
+            char ch = sentence.charAt(i);
+            if("aeiou".indexOf(ch) != -1){
+                sb.append(Character.toUpperCase(ch));
+            }
+            else{
+                sb.append(ch);
+            }
+        }
+        return sb.toString().trim();
+    }
     public static void main(String[] args) {
         System.out.println(Reverse_word("My name is billa!!!   "));
+        System.out.println(Vowel_Capital("my name is jevaa kharthik n"));
         
     }
     
