fc302b090cb45def41468889391e354fb294da43
Jevaa Kharthik N <113284998+Jevaa-kharthik@users.noreply.github.com>
2024-09-03 20:17:53 +0530

    Interview Question

--- Maventic1.java
+++ Maventic1.java
@@ -1,23 +1,34 @@
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Maventic1 {
     public static void characterArraySort(char[] arr){
-        int[] array = new int[arr.length];
+        Map<Character, Integer> map = new HashMap<>();
         for(int i = 0; i < arr.length; i++){
-            int number = arr[i] - 'a';
-            array[i] = number;
+            map.put(arr[i],map.getOrDefault(arr[i], 0) + 1);
         }
-        Arrays.sort(array);
-        for(int i = 0; i < array.length; i++){
-            char ch = (char) (array[i] + 'a');
-            arr[i] = ch;
-        }
+
+        Character chArray[] = new Character[arr.length];
         for(int i = 0; i < arr.length; i++){
-            System.out.println(arr[i]);
+            chArray[i] = arr[i];
+        }
+        Arrays.sort(chArray,(a,b) -> {
+            if(map.get(a) == map.get(b)) return a-b; 
+            return map.get(b) - map.get(a);
+        });
+
+        for(int i = 0; i < chArray.length; i++){
+            System.out.println(chArray[i]);
         }
     }
+        
+
+
+
+    
     public static void main(String[] args) {
-        char[] arr = {'e', 'd', 'c', 'b', 'a'};
+        char[] arr = {'a' ,'b' , 'c' , 'd' , 'a' ,'b' ,'d' ,'e' ,'d' ,'e' ,'d'};
         characterArraySort(arr);
     }
     
